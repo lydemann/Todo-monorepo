870d98e83187c8ef032dc7b69d2cc62ac8a3265d
Christian Ludemann <ext.christian.lude@ef.com>
2019-07-03 20:27:23 +0200

    chore: update to ng8

--- apps/todo-app-e2e/src/integration/todo.spec.ts
+++ apps/todo-app-e2e/src/integration/todo.spec.ts
@@ -1,26 +1,26 @@
 import { TodoPage } from '../support/todo.po';
 
 const todoList = [
-  {
-    id: 'task1',
-    title: 'Do1',
-    description: 'Remember to buy milk'
-  },
-  {
-    id: 'task2',
-    title: 'Do2',
-    description: 'Remember to work out'
-  }
+	{
+		id: 'task1',
+		title: 'Do1',
+		description: 'Remember to buy milk',
+	},
+	{
+		id: 'task2',
+		title: 'Do2',
+		description: 'Remember to work out',
+	},
 ];
 describe('Todo', () => {
-  beforeEach(() => {
-    cy.server();
-    cy.route('http://localhost:8080/api/todo-list', todoList);
+	beforeEach(() => {
+		cy.server();
+		cy.route('http://localhost:8080/api/todo-list', todoList);
 
-    cy.visit('/');
-  });
+		cy.visit('/');
+	});
 
-  it('should should show todo items', () => {
-    TodoPage.checkForTodos(todoList);
-  });
+	it('should should show todo items', () => {
+		TodoPage.checkForTodos(todoList);
+	});
 });
