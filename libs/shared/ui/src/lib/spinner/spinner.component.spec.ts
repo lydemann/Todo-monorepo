24096cdc64b1f2ff686caca003161c3ee6cff699
Christian Lüdemann <christian@christianlydemann.com>
2019-09-08 11:09:01 +0200

    test: run test on affected on push

--- libs/shared/ui/src/lib/spinner/spinner.component.spec.ts
+++ libs/shared/ui/src/lib/spinner/spinner.component.spec.ts
@@ -1,6 +1,6 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { SpinnerComponent } from '@shared-lib/spinner/spinner.component';
+
+import { SpinnerComponent } from './spinner.component';
 
 describe('SpinnerComponent', () => {
 	let component: SpinnerComponent;
