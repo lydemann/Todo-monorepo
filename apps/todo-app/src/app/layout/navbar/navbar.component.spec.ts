0e92d9093c236821ea330334c0d34ca6a948e791
Christian Lüdemann <christianlydemann@gmail.com>
2019-09-10 13:49:09 +0200

    test: fixed tests

--- apps/todo-app/src/app/layout/navbar/navbar.component.spec.ts
+++ apps/todo-app/src/app/layout/navbar/navbar.component.spec.ts
@@ -1,66 +1,36 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import {
+	createComponentFactory,
+	mockProvider,
+	Spectator,
+} from '@ngneat/spectator';
 import { TranslateModule } from '@ngx-translate/core';
 
-import { AppComponent } from '@todo-app/app.component';
-import { appRouterModule } from '@todo-app/app.routes';
-import { CoreModule } from '@todo-app/core/core.module';
-import { FooterComponent } from '@todo-app/footer/footer.component';
 import { NavbarComponent } from '@todo-app/layout/navbar/navbar.component';
-import { TodoItemListRowComponent } from '@todo-app/shared/todo-item-list-row/todo-item-list-row.component';
-import { TodoListCompletedComponent } from '@todo-app/todo-list-completed/todo-list-completed.component';
-import { AddTodoComponent } from '@todo-app/todo-list/add-todo/add-todo.component';
-import { TodoListComponent } from '@todo-app/todo-list/todo-list.component';
-import {
-	FeatureToggleDirective,
-	FeatureToggleService,
-} from '@todo/shared/util-feature-toggle';
+import { SharedModule } from '@todo-app/shared/shared.module';
+import { FeatureToggleService } from '@todo/shared/util-feature-toggle';
 
 describe('NavbarComponent', () => {
-	let component: NavbarComponent;
-	let fixture: ComponentFixture<NavbarComponent>;
-
-	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [
-				AppComponent,
-				NavbarComponent,
-				TodoListComponent,
-				TodoItemListRowComponent,
-				FooterComponent,
-				AddTodoComponent,
-				TodoListCompletedComponent,
-				FeatureToggleDirective,
-			],
-			imports: [
-				BrowserModule,
-				NgbModule,
-				TranslateModule.forRoot(),
-				FormsModule,
-				CoreModule,
-				HttpClientModule,
-				appRouterModule,
-			],
-			providers: [
-				{ provide: APP_BASE_HREF, useValue: '/' },
-				FeatureToggleService,
-			],
-			schemas: [NO_ERRORS_SCHEMA],
-		}).compileComponents();
-	}));
-
-	beforeEach(() => {
-		fixture = TestBed.createComponent(NavbarComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
+	let spectator: Spectator<NavbarComponent>;
+	const createComponent = createComponentFactory({
+		component: NavbarComponent,
+		imports: [
+			FormsModule,
+			RouterModule.forRoot([]),
+			TranslateModule.forRoot(),
+			SharedModule,
+		],
+		providers: [
+			mockProvider(FeatureToggleService, {
+				hasFlags: () => true,
+			}),
+		],
 	});
 
-	it('should create', () => {
-		expect(component).toBeTruthy();
+	beforeEach(() => (spectator = createComponent()));
+
+	it('should contain nav bar items', () => {
+		expect(spectator.queryAll('.nav-item').length).toBe(3);
 	});
 });
--- apps/todo-app/src/app/layout/navbar/navbar.component.ts
+++ apps/todo-app/src/app/layout/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
 	selector: 'app-navbar',
 	templateUrl: './navbar.component.html',
-	styleUrls: ['./navbar.component.css'],
+	styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
 	constructor(public translate: TranslateService) {}
