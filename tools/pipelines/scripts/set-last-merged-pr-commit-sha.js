96cf0539b42e6d3fc322f5e33be7f7661e66d3ca
Christian Lüdemann <christian@christianlydemann.com>
2020-04-08 20:26:12 +0200

    chore: set last pr sha

---
+++ tools/pipelines/scripts/set-last-merged-pr-commit-sha.js
@@ -0,0 +1,58 @@
+const octokit = require('@octokit/rest');
+
+async function lastMergedPRCommitSha(octokit) {
+	const result = await octokit
+		.graphql({
+			query: `
+      query { 
+        repository(owner: "lydemann", name: "Todo-monorepo") {
+          pullRequests(states: MERGED, last: 1) {
+            nodes {
+              url,
+              commits(last:1) {
+                edges {
+                  node {
+                    commit {
+                      oid
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+		})
+		.catch(error => {
+			console.error(JSON.stringify(error));
+		});
+
+	const commitSha =
+		result.repository.pullRequests.nodes[0].commits.edges[0].node.commit.oid;
+
+	return commitSha;
+}
+
+async function run() {
+	const token =
+		process.env.GITHUB_TOKEN || '856a34abd5236b21a7ada7273f0701c729175f35';
+	if (!token) {
+		console.log(`'env.GITHUB_TOKEN' not found - exiting...`);
+		return;
+	}
+
+	const octo = new octokit.Octokit({
+		auth: `token ${token}`,
+	});
+
+	const commitSha = await lastMergedPRCommitSha(octo);
+
+	console.log(`Found commitSha: ${commitSha}`);
+
+	console.log(
+		`##vso[task.setvariable variable=LAST_PR_SHA;isOutput=true]${commitSha}`,
+	);
+}
+
+run();
