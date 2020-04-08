048e637ca9f03fecfb3c658d572f0173e623f7db
Christian Lüdemann <christian@christianlydemann.com>
2020-04-08 15:43:39 +0200

    chore: add comment

--- apps/todo-service/src/app/app.controller.ts
+++ apps/todo-service/src/app/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
+// Routes are set up here
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
