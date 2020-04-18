bf8fadae0294e39a31db517706c48600738d8e95
Christian Lüdemann <christian@christianlydemann.com>
2020-04-18 15:27:11 +0200

    task/test-caching (#20)
    * chore: update cache resolve
    
    * chore: fix format
    
    * chore: trigger service affect
    
    * build: cache node modules
    
    * chore: fix template parameter
    
    * build: fix cypress parameter
    
    * chore: install if cache not restored
    
    * build: fix install
    
    * chore: fix syntax
    
    * chore(todo-service): some comment
    
    * chore: add cache hit var
    
    * chore: cache hit var
    
    * build: cache yarn cache
    
    * chore: fix install
    
    * chore: fix syntax
    
    * chore: publish coverage
    
    * chore: add pr test command
    
    * chore: add coverage
    
    * chore: fix test
    
    * chore: fix tests
    
    * chore: fix junit config
    
    * chore: fix yarn path
    
    * chore: comment in app-service
    
    * chore: fix cache 1
    
    * chore: fix cache 2

--- apps/todo-service/src/app/app.controller.ts
+++ apps/todo-service/src/app/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
-// Routes are set up in this file
+// Routes are set up here
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
