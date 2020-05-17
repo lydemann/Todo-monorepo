8721c0d43363cb31c48194443c5e7d34b03d5c6e
Christian Lüdemann <christian@christianlydemann.com>
2020-05-17 16:44:33 +0200

    test: fixed tests

--- libs/todo-app-lib/src/lib/todo-list/state/todo-list.selector.spec.ts
+++ libs/todo-app-lib/src/lib/todo-list/state/todo-list.selector.spec.ts
@@ -1,5 +1,9 @@
 import { TodoItem } from '@todo/shared/todo-interfaces';
-import { todoListInitState, TodoListState } from './todo-list.model';
+import {
+	todoListAdapter,
+	todoListInitState,
+	TodoListState,
+} from './todo-list.model';
 import {
 	selectCompletedTodos,
 	selectIsAddingTodo,
@@ -13,10 +17,11 @@ describe('Todo list selectors', () => {
 		it('should return the todoList', () => {
 			const todos = [new TodoItem('todo1', 'todo1')];
 
-			const todoListState = {
-				todos,
+			let todoListState = {
 				isLoading: true,
 			} as TodoListState;
+
+			todoListState = todoListAdapter.setAll(todos, todoListState);
 
 			expect(selectTodoList.projector(todoListState)).toEqual(todos);
 		});
