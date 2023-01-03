5e9095b02d1a24bbda5d6163ad4e01131d6beec1
Christian Lüdemann <christian@christianlydemann.com>
2023-01-03 14:07:26 +0100

    test: fix

--- libs/shared/ui/src/test.ts
+++ libs/shared/ui/src/test.ts
@@ -1,6 +1,5 @@
 /* eslint-disable */
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
 import 'zone.js/dist/zone-testing';
