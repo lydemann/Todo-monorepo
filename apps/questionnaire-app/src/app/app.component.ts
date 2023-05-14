0fb7ade7d60b274014dff8c1798ac98d0818dfbd
Christian Lüdemann <christian@christianlydemann.com>
2023-05-14 10:40:25 +0200

    fix: questionnaire (#39)

--- apps/questionnaire-app/src/app/app.component.ts
+++ apps/questionnaire-app/src/app/app.component.ts
@@ -9,9 +9,6 @@ import { QuestionnaireRoot } from './questionnaire.model';
 	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-	// public questionnaire = (questionnaireMock as QuestionnaireRoot)[
-	// 	'questionnaire'
-	// ];
-	// TODO: fix Should not import the named export
-	public questionnaire = {};
+	public questionnaire = (questionnaireMock as QuestionnaireRoot)
+		?.questionnaire;
 }
