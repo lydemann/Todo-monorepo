225db558371986c1e71cf3c01ef3506cb59146c2
Christian Lüdemann <christian@christianlydemann.com>
2023-08-16 18:39:36 +0200

    ci: fix

--- tools/pipelines/scripts/create-affected-apps-txt.ps1
+++ tools/pipelines/scripts/create-affected-apps-txt.ps1
@@ -1,10 +1,10 @@
-$AffectedAppsObj = Invoke-Expression 'npm run affected:apps -- --base=origin/master --head=HEAD';
-$AffectedAppsString = $AffectedAppsObj[4];
+$AffectedAppsObj = Invoke-Expression 'nx print-affected --type=app --select=projects';
+$AffectedAppsString = $AffectedAppsObj;
 
 if ($AffectedAppsString -eq "") {
     Write-Host "No affected apps. Tagging with all apps.";
-    $AffectedAppsObj = Invoke-Expression 'npm run affected:apps -- --all';
-    $AffectedAppsString = $AffectedAppsObj[4];
+    $AffectedAppsObj = Invoke-Expression 'npx nx print-affected --type=app --select=projects';
+    $AffectedAppsString = $AffectedAppsObj;
 }
 
 "Affected apps: $AffectedAppsString";
