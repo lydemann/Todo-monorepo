3d1f3a8d70a4ecc3863177d5c0e34cc1ac5bd7b7
Christian Lüdemann <christian@christianlydemann.com>
2023-08-18 14:23:21 +0200

    fix: format

--- apps/todo-app/cypress/support/commands.ts
+++ apps/todo-app/cypress/support/commands.ts
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-import { mount } from 'cypress/angular'
+import { mount } from 'cypress/angular';
 
 // ***********************************************
 // This example commands.ts shows you how to
@@ -12,22 +12,22 @@ import { mount } from 'cypress/angular'
 // ***********************************************
 
 declare global {
-// eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace Cypress {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    interface Chainable<Subject> {
-      login(email: string, password: string): void;
-      mount: typeof mount;
-    }
-  }
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace Cypress {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		interface Chainable<Subject> {
+			login(email: string, password: string): void;
+			mount: typeof mount;
+		}
+	}
 }
 
-Cypress.Commands.add('mount', mount)
+Cypress.Commands.add('mount', mount);
 
 //
 // -- This is a parent command --
 Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
+	console.log('Custom command example: Login', email, password);
 });
 //
 // -- This is a child command --
