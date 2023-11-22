a58b6bd5dd1ae85d02c008736ee79be5036261e7
Christian Lüdemann <christian@christianlydemann.com>
2023-11-22 15:40:27 +0100

    chore: fix lint

--- libs/shared/ui/src/lib/textarea/textarea.component.ts
+++ libs/shared/ui/src/lib/textarea/textarea.component.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
@@ -9,7 +9,6 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 	providers: [
 		{
 			provide: NG_VALUE_ACCESSOR,
-			// tslint:disable-next-line: no-forward-ref
 			useExisting: forwardRef(() => TextareaComponent),
 			multi: true,
 		},
@@ -22,10 +21,10 @@ export class TextareaComponent implements ControlValueAccessor {
 	public value = '';
 
 	// tslint:disable-next-line: no-empty
-	public propagateChange: any = _ => {};
+	public propagateChange: any = _ => { };
 
 	// tslint:disable-next-line: no-empty
-	public onTouched: any = _ => {};
+	public onTouched: any = _ => { };
 
 	public onChange(event) {
 		this.propagateChange(event);
