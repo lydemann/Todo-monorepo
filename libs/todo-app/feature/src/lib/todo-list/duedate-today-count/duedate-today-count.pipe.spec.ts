9ef901af078bbfc0840f461e1a4d3c2d3868e18e
Christian Lüdemann <christian@christianlydemann.com>
2023-12-13 09:14:02 -0300

    test: fix

--- libs/todo-app/feature/src/lib/todo-list/duedate-today-count/duedate-today-count.pipe.spec.ts
+++ libs/todo-app/feature/src/lib/todo-list/duedate-today-count/duedate-today-count.pipe.spec.ts
@@ -6,8 +6,8 @@ describe('DuedateTodayCountPipe', () => {
 		it('have two task that are due today', () => {
 			const pipe = new DuedateTodayCountPipe();
 			const todos = [
-				new TodoItem('', '', new Date().toISOString().slice(0, 10)),
-				new TodoItem('', '', new Date().toISOString().slice(0, 10)),
+				new TodoItem('', '', new Date().toISOString()),
+				new TodoItem('', '', new Date().toISOString()),
 				new TodoItem('', ''),
 			];
 			const res = pipe.transform(todos);
