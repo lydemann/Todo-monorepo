535fb43592fe4ba731695f1786ea14f616c5b05c
Christian Lüdemann <christian@christianlydemann.com>
2023-12-18 14:40:42 -0300

    fix: lint

--- apps/logging-service/src/app/app.service.ts
+++ apps/logging-service/src/app/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	public postLog(data: any) {
 		// tslint:disable-next-line: no-console
 		console.log(JSON.stringify(data));
