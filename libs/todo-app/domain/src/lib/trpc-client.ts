55d4027c4a736889e1b116d1eff67637626874f0
Christian Lüdemann <christian@christianlydemann.com>
2024-03-29 13:21:02 +0100

    fix: typing

--- libs/todo-app/domain/src/lib/trpc-client.ts
+++ libs/todo-app/domain/src/lib/trpc-client.ts
@@ -1,14 +1,17 @@
-import { TodoTrpcRouter } from '@todo/todo-app/domain/trpc-server';
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import { createTRPCProxyClient, httpBatchLink, loggerLink } from '@trpc/client';
 /**
- * We only import the `AppRouter` type from the server - this is not available at runtime
+ * We only import the `TodoTrpcRouter` type from the server - this is not available at runtime
  */
+import type { TodoTrpcRouter } from '@todo/todo-app/domain/trpc-server';
+import { DataTransformerOptions } from '@trpc/server';
 
 // Initialize the tRPC client
 export const trpc = createTRPCProxyClient<TodoTrpcRouter>({
 	links: [
+		loggerLink(),
 		httpBatchLink({
 			url: 'http://localhost:3333/api',
 		}),
 	],
-} as never);
+	transformer: {} as DataTransformerOptions,
+});
